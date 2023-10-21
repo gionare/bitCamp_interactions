115862215ca3f0eeac807013fa7f9f54ded991f0
gionare <gionare93@gmail.com>
2023-10-21 01:37:50 -0400

    added Find Missing Number in array

---
+++ stage_1/arrays/findN.js
@@ -0,0 +1,16 @@
+// Find Missing Number: 
+// Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, 
+// find the missing number. 
+// For example, given the array [3, 0, 1], the function should return 2.
+
+const array = [3, 0, 1];
+
+function findMissingN (array){
+    const n = array.length;
+    const expectedSUm = (n * (n + 1)/2);
+    //console.log(expectedSUm);
+    const actualSum = array.reduce((sum, num) => sum + num, 0); //შეეკითხე .reduce( , 0)
+    //console.log(actualSum);
+    return expectedSUm - actualSum;
+}
+console.log(findMissingN(array));
