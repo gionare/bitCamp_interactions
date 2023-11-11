00d82853a6003852d32bbfd6c1664a8b19333c37
gionare <gionare93@gmail.com>
2023-11-10 19:15:44 -0500

    Finished Dom

--- stage_1/dom/script.js
+++ stage_1/dom/script.js
@@ -1,55 +1,53 @@
-'use strict';
+"use strict";
 // // Exercise 1: Changing Text Content
-// // <p> element. 
-//changes text content of the <p> element to "Hello, DOM!".
-
-
+const para = document.querySelector(".paragraph");
 // შევცვალოთ არსებული textContent
 function changeTextContent() {
-    const para = document.querySelector('.paragraph'); 
-    para.textContent = 'Hello DOM!';
+  para.textContent = "Hello DOM!";
 }
-
-
+changeTextContent();
 
 // // Exercise 2: Changing Styles
-// <button> element. 
-// changes the background color of the <button> to red and the text color to white.
+const newBtnColor = document.querySelector(".custom-button");
 
-function changeBtnColor(){
-    const para = document.querySelector('.paragraph');
-    const newBtnColor = document.querySelector('.custom-button');
-
-    para.style.color = 'green';
-    newBtnColor.style.backgroundColor = 'red';
+function changeBtnColor() {
+  para.style.color = "green";
+  newBtnColor.style.backgroundColor = "white";
 }
+newBtnColor.addEventListener("click", changeBtnColor);
+// shegvidzlia davamatot funqcia 2e parametrad
 
 // // Exercise 3: Creating Elements
-// // Create an HTML page with an empty <div> element. 
-// Write a JavaScript function that, when called, creates a new <p> element 
-// with the text content "New paragraph" and appends it as a child to the <div>.
+//createElement(); createTextNode(); appendChild(); append() 
+const newPara = document.createElement("p");
+const node = document.createTextNode("This is new paragraph");
 
 function newParagraph() {
-    
-
-    const newPara = document.createElement("p");
-    const node = document.createTextNode("This is new paragraph");
-    newPara.appendChild(node);
-
-    const element = document.getElementById("myDiv");
-    element.appendChild(newPara);
-
+  newPara.appendChild(node);
+  const element = document.getElementById("myDiv");
+  element.appendChild(newPara);
 }
-newParagraph() //call the function
-
+newParagraph(); //call the function
 
 // // Exercise 4: Event Handling
-// // Create an HTML page with a <button> element. 
-// Write a JavaScript function that, when the button is clicked, 
-// displays an alert with the message "Button clicked!".
+let button = document.querySelector(".custom-button");
 
-// // Exercise 5: Form Validation
-// // Create an HTML page with a <form> element containing an input field and a submit button. 
-// Write a JavaScript function that, when the form is submitted, validates the input field. 
-// If the input is empty, display an error message next to the input field. 
-// If the input is not empty, display an alert with the message "Form submitted!".
+button.addEventListener("click", () => {
+  alert("button Clicked!");
+});
+
+// Exercise 5: Form Validation. 
+// addEventListener for form btn if it inlcudes or not any string 
+let inputForm = document.querySelector("input");
+console.log(inputForm.value);
+let formButton = document.querySelector("#btn");
+console.log(formButton);
+
+formButton.addEventListener("click", (formButtonPreventer) => {
+    formButtonPreventer.preventDefault(); //prevent to refresh on click for button
+    console.log(formButtonPreventer);
+  if (inputForm.value.length != 0) {
+    alert("Form submitted!");
+  }
+});
+
