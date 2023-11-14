2b6c393a86b17113ff6e5095d69817c607dd8dca
gionare <gionare93@gmail.com>
2023-11-13 19:46:10 -0500

    fetch

---
+++ asyncProg/fetching.js
@@ -0,0 +1,34 @@
+// 3. Write a function that runs after 2 seconds when executed,
+//      counts from 1 to 10 and then fetches th github user data
+
+// fetching user data
+
+async function getGithubUser(user) {
+    const response = await fetch(`https://api.github.com/users/${user}`);
+    const data = await response.json();
+    console.log(data.login);
+  }
+  
+  function mainFunction() {
+    let counter = 0;
+  
+    let interval = setInterval(() => {
+      counter++;
+      console.log(counter);
+  
+      if (counter >= 10) {
+        clearInterval(interval);
+        // Wait for the fetch operation to complete before moving forward
+        getGithubUser("gionare")
+          .then(() => {
+            // Do something else after fetching user data if needed
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }
+    }, 2000); // Changed to 2000 milliseconds (2 seconds)
+  }
+  
+  setTimeout(mainFunction, 10);
+  
