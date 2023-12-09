bc69203c09941380f4ee78c75835aee9b952c174
gionare <gionare93@gmail.com>
2023-12-09 13:40:51 -0500

    adding new shoppingCart Class

--- OOP/onlineShopping.js
+++ OOP/onlineShopping.js
@@ -38,7 +38,7 @@ User Class:
 Create function called order, that will be used to empty shopping cart and give the products to user
 
 */
-
+// _____________________ P R O D U C T  C L A S S ________________________
 class product {
   #productId;
   #productName;
@@ -49,15 +49,68 @@ class product {
     this.#productName = productName;
     this.#price = price;
   }
-  // nee methods to ritch private class
-  get getProductId() {
+  // need methods to ritch private class
+  get productId() {
     return this.#productId;
   }
-  getUserId() {
+  get productName() {
+    return this.#productName;
+  }
+  getprice() {
     return this.#price;
+  }
+  // __ Return all properties, by creating new object in method__
+  getProductInfo() {
+    return {
+      productId: this.#productId,
+      productName: this.#productName,
+      price: this.#price,
+    };
   }
 }
 
 const product1 = new product("1", "pillow", 40);
-console.log(product1.getProductId);
-console.log(product1.getUserId());
+const product2 = new product("2", "LEGO car", 100);
+const product3 = new product("3", "MI TV LUX", 4000);
+// console.log(product1.productId);
+// console.log(product1.productName);
+// console.log(product1.getUserId());
+console.log(product1.getProductInfo());
+console.log(product2.getProductInfo());
+
+// ________________ S H O P P I N G C A R T  C L A S S  ____________________
+class shoppingCart {
+  #cartId
+  #items
+  constructor (cartId, item) {
+    this.#cartId = cartId;
+    this.#items = [];
+  }
+  get cartId () {
+    return this.#cartId;
+  }
+  get items () {
+    return this.#items;
+  }
+  // push items in addItem[]
+  addItem (product) {
+    this.items.push(product);
+  }
+  // need logic to take out certuin element from []
+  // 1. using splice(startIndex, howMany), so we need to know start index
+  // 2. 
+  removeItem(product) {
+    const index = this.items.indexOf(product);
+    if (index !== -1){
+    this.items.splice(index, 1)
+    }
+  }
+// __ - calculateTotal(): Calculates and returns the total price of items in the cart. __
+  calculateTotal () {
+    return this.#items.reduce((accumulator, product) => accumulator + product.getprice() ,0);
+  }
+
+
+}
+const shoppingCart1 = new shoppingCart("1");
+console.log(shoppingCart1.items());
